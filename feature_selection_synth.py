0c89f1639f2b26c045d65acc6783c5039576e408
Ian Pourlotfali <pourloi@wwu.edu>
2026-05-29 22:20:53 -0700

    working nearest neighbor, confirmed with scikitlearn

--- feature_selection_synth.py
+++ feature_selection_synth.py
@@ -9,7 +9,30 @@ def load_data(name):
     features = (features - means) / stds
     return labels, features
 
+def nearest_neighbor(labels, features, selected_feats):
+    n_data = len(labels)
+    data = features[:, list(selected_feats)]
+    correct_pred = 0
+
+    for i in range(len(labels)):
+        distances = np.sqrt(np.sum((data - data[i]) ** 2, axis=1))
+        distances[i] = float('inf')
+        nearest = np.argmin(distances)
+        if labels[nearest] == labels [i]:
+            correct_pred = correct_pred + 1
+    
+    return correct_pred / len(labels)
+
+
+# Used a built in scikit learn KNN classifier on the
+# small dataset to check if my NN implementation was correct
+# and it was as both got 79.1% accuracy
+
+
+
+
 if __name__ == "__main__":
     name = input("Enter the name of the data file: ")
     labels, features = load_data(name)
-    print(features.shape)
+    selected_features = set(range(features.shape[1]))
+    print(nearest_neighbor(labels, features, selected_features))
